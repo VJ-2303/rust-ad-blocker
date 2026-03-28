27a4e1d8d1000ff20529210fcad3499a08b78ef6
VJ-2303 <vanaraj1018@gmail.com>
2026-03-28 09:42:01 +0530

    Added TTL Expiration in cache

--- src/dns/cache.rs
+++ src/dns/cache.rs
@@ -1,10 +1,21 @@
-use std::{collections::HashMap, sync::Arc};
+use std::{
+    collections::HashMap,
+    sync::Arc,
+    time::{Duration, Instant},
+};
 
+use anyhow::Ok;
 use tokio::sync::RwLock;
 
 #[derive(Debug, Clone)]
 pub struct Cache {
-    store: Arc<RwLock<HashMap<Vec<u8>, Vec<u8>>>>,
+    store: Arc<RwLock<HashMap<Vec<u8>, CacheEntry>>>,
+}
+
+#[derive(Debug, Clone)]
+pub struct CacheEntry {
+    pub response_bytes: Vec<u8>,
+    pub expires_at: Instant,
 }
 
 impl Cache {
@@ -14,11 +25,14 @@ impl Cache {
         }
     }
     pub async fn get(&self, domain: &[u8], original_id: &[u8]) -> Option<Vec<u8>> {
-        let store = self.store.read().await;
+        let mut store = self.store.write().await;
 
-        if let Some(cached_response) = store.get(domain) {
-            let mut response = cached_response.clone();
-
+        if let Some(cached_entry) = store.get(domain) {
+            if Instant::now() > cached_entry.expires_at {
+                store.remove(domain);
+                return None;
+            }
+            let mut response = cached_entry.response_bytes.clone();
             response[0] = original_id[0];
             response[1] = original_id[1];
 
@@ -29,6 +43,10 @@ impl Cache {
     pub async fn put(&self, domain: Vec<u8>, response_bytes: Vec<u8>) {
         let mut store = self.store.write().await;
 
-        store.insert(domain, response_bytes);
+        let entry = CacheEntry {
+            response_bytes,
+            expires_at: Instant::now() + Duration::from_secs(300),
+        };
+        store.insert(domain, entry);
     }
 }
